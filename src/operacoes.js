3cd7a3e86b786706ab2ecba43f9eaa61d0fca341
Clara Pietra <clara.viana@alunos.sc.senac.br>
2023-02-22 13:59:34 -0300

    test

--- src/operacoes.js
+++ src/operacoes.js
@@ -4,8 +4,44 @@ function sum (num1, num2) {
     }
     return num1+num2
 }
-
-module.exports ={
-    sum
+function subtracao(num1, num2) {
+    if (isNaN(num1) || isNaN (num2)){
+        throw "Não é número"
+    }
+    return num1-num2
 }
-clara
+function multiplicacao(num1, num2) {
+    if (isNaN(num1) || isNaN (num2)){
+        throw "Não é número"
+    }
+    return num1*num2
+}
+
+function divisao(num1, num2) {
+    if (isNaN(num1) || isNaN (num2)){
+        throw "Não é número"
+    }
+    return num1/num2
+}
+function raiz(num1) {
+   if (isNaN(num1) < 0){
+    throw "Não é número" 
+}
+    return Math.sqrt(num1)
+}
+function potencia(num1, num2) {
+    if (isNaN(num1,num2) < 0){
+     throw "Não é número" 
+ }
+     return Math.pow(num1,num2)
+ }
+ 
+ module.exports ={
+     sum, 
+     subtracao, 
+     multiplicacao, 
+     divisao, 
+     raiz,
+      potencia
+ }
+ 
